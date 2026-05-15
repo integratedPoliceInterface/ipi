215826a339ad3bcdd5565fc66598e22219166b62
pablodev060 <pablodev060@gmail.com>
2026-05-15 10:28:17 -0300

    15_05_2026 10:25 Implementando restante da funcionalidade de mapas offline

---
+++ www/sw.js
@@ -0,0 +1,37 @@
+const CACHE_ESTATICO = 'ipi-appa-v1';
+const URLS_ESTATICOS = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/db.js',
+  './js/conectividade.js',
+  './js/servicoBusca.js',
+  './js/servicoSincronizacao.js',
+  './js/app.js',
+  './js/aparencia.js',
+  './js/mapa.js',
+  './js/goias-municipios.js',
+  './js/configSupabase.js',
+  './js/vendor/protomaps-leaflet.js',
+  './js/vendor/pmtiles.js',
+  './img/image.png',
+  './img/icone-policial.png',
+  './img/Brasao_PMGO.png',
+  './img/brasao-pmgo-rural.webp'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_ESTATICO).then((c) => c.addAll(URLS_ESTATICOS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => new Response('Offline', { status: 503 })))
+  );
+});
